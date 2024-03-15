1696783855776521dfdcf91c96ed42e35701b9e2
Mykhailo Petrov <mykhailo.petrov@schindler.com>
2024-03-15 21:28:22 +0200

    MongoDB with mongoose

--- server.js
+++ server.js
@@ -1,98 +1,24 @@
 const express = require('express')
-const { ObjectId } = require('mongodb')
-const { connectToDb, getDb } = require('./db')
+const mongoose = require('mongoose')
+const movieRouter = require('./routes/movie-routes')
 
 const PORT = 3000
+const URL =
+	'mongodb+srv://mixeys2008:User123@books-store.zaptjfx.mongodb.net/moviebox?retryWrites=true&w=majority&appName=books-store'
 
 const app = express()
 app.use(express.json())
+app.use(movieRouter)
 
-let db
-
-connectToDb((err) => {
-	if (!err) {
-		app.listen(PORT, (err) => {
-			err ? console.log('error: ', err) : console.log(`Listening port ${PORT}`)
-		})
-		db = getDb()
-	} else {
+mongoose
+	.connect(URL)
+	.then(() => {
+		console.log('Connected to MongoDB')
+	})
+	.catch((err) => {
 		console.log(`DB connected error: ${err}`)
-	}
-})
+	})
 
-const handleError = (res, error) => {
-	res.status(500).json({ error })
-}
-
-app.get('/movies', (req, res) => {
-	const movies = []
-
-	db.collection('movies')
-		.find()
-		.sort({ title: 1 })
-		.forEach((movie) => {
-			movies.push(movie)
-		})
-		.then(() => {
-			res.status(200).json(movies)
-		})
-		.catch(() => {
-			handleError(res, 'Something goes wrong...')
-		})
-})
-
-app.get('/movies/:id', (req, res) => {
-	if (ObjectId.isValid(req.params.id)) {
-		db.collection('movies')
-			.findOne({ _id: new ObjectId(req.params.id) })
-			.then((doc) => {
-				res.status(200).json(doc)
-			})
-			.catch(() => {
-				handleError(res, 'Something goes wrong...')
-			})
-	} else {
-		handleError(res, 'Wrong id')
-	}
-})
-
-app.delete('/movies/:id', (req, res) => {
-	if (ObjectId.isValid(req.params.id)) {
-		db.collection('movies')
-			.deleteOne({ _id: new ObjectId(req.params.id) })
-			.then((result) => {
-				res.status(200).json(result)
-			})
-			.catch(() => {
-				handleError(res, 'Something goes wrong...')
-			})
-	} else {
-		handleError(res, 'Wrong id')
-	}
-})
-
-app.post('/movies', (req, res) => {
-	db.collection('movies')
-		.insertOne(req.body)
-		.then((result) => {
-			res.status(201).json(result)
-		})
-		.catch(() => {
-			handleError(res, 'Something goes wrong...')
-		})
-})
-
-app.patch('/movies/:id', (req, res) => {
-	if (ObjectId.isValid(req.params.id)) {
-		db.collection('movies')
-			.updateOne({ _id: new ObjectId(req.params.id) }, { $set: req.body })
-			.then((result) => {
-				res.status(200).json(result)
-			})
-			.catch(() => {
-				handleError(res, 'Something goes wrong...')
-			})
-	} else {
-		handleError(res, 'Wrong id')
-	}
+app.listen(PORT, (err) => {
+	err ? console.log('error: ', err) : console.log(`Listening port ${PORT}`)
 })
